ce41d801c287ca0975e8d1e296b259c231ab7ad3
Eddy Almanza <a01568010@gmail.com>
2022-10-26 21:02:16 -0600

    Added sidebar links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,16 @@
 import './sidebar.scss'
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import { useNavigate } from "react-router-dom"
 
 const Sidebar = () => {
+    let navigate = useNavigate();
+
+    const changePage = (url) => {
+        navigate(url);
+    }
+
+    
     return (
         <div className='sidebar'>
             <div className='top'>
@@ -11,11 +19,11 @@ const Sidebar = () => {
             <hr />
             <div className='center'>
                 <ul>
-                    <li>
+                    <li onClick={() => changePage("/dashboard")}>
                         <DashboardIcon className='icon'/>
                         <span>Dashboard</span>
                     </li>
-                    <li>
+                    <li onClick={() => changePage("/register")}>
                         <PersonAddIcon  className='icon'/>
                         <span>Registrar Usuario</span>
                     </li>
