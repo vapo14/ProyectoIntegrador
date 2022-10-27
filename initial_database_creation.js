4e67a25c644c5224473ae9f15cb2a0a334f9c772
Eddy Almanza <a01568010@gmail.com>
2022-10-26 20:46:35 -0600

    Fixed Initial database

--- initial_database_creation.js
+++ initial_database_creation.js
@@ -1,15 +1,26 @@
-const AppDAO = require("./dao/dao");
-const RolesRepository = require("./dao/roles_repository");
-const UserRepository = require("./dao/user_repository");
-const UserRolesRepository = require("./dao/user_roles_repository");
-const ReservationRepository = require("./dao/reservation_repository");
-const GuestRepository = require("./dao/guest_repository");
+const bcrypt = require('bcryptjs')
+const AppDAO = require("./DAO/dao");
+const RolesRepository = require("./DAO/roles_repository");
+const UserRepository = require("./DAO/user_repository");
+const UserRolesRepository = require("./DAO/user_roles_repository");
+const ReservationRepository = require("./DAO/reservation_repository");
+const GuestRepository = require("./DAO/guest_repository");
 
-const GeneratePassword = require("./src/security/generatePassword");
+const GetRandomString = (length) => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for( var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
 
 const dao = new AppDAO("./database.sqlite3");
 var password = "password123";
-var passwordObj = GeneratePassword(password);
+var salt = GetRandomString(20);
+var passwordObj = bcrypt.hashSync(password + salt)
 
 const superUser = {
   username: "superuser",
