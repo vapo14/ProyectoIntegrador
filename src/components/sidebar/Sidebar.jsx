19dc77ea265fdf961018dbc67627d0fd15957199
Víctor Padrón <49078518+vapo14@users.noreply.github.com>
2022-11-30 19:39:11 -0600

    feat: added login and logout functionality (PI-35)

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,43 +1,58 @@
-import './sidebar.scss'
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import { useNavigate } from "react-router-dom"
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Sidebar = () => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
+  const { logout, authed } = useAuth();
 
-    const changePage = (url) => {
-        navigate(url);
-    }
+  const changePage = (url) => {
+    navigate(url);
+  };
 
-    
-    return (
-        <div className='sidebar'>
-            <div className='top'>
-                <span className='logo'>La Gloria de Calvillo</span>
-            </div>
-            <hr />
-            <div className='center'>
-                <ul>
-                    <li onClick={() => changePage("/dashboard")}>
-                        <DashboardIcon className='icon'/>
-                        <span>Dashboard</span>
-                    </li>
-                    <li onClick={() => changePage("/register")}>
-                        <PersonAddIcon  className='icon'/>
-                        <span>Registrar Usuario</span>
-                    </li>
-                    <li onClick={() => changePage("/crearHabitacion")}>
-                        <span>Crear habitacion</span>
-                    </li>
-                    <li onClick={() => changePage("/calendar")}>
-                        <span>Calendario</span>
-                    </li>
-                </ul>
-            </div>
-            <div className='bottom'></div>
-        </div>
-    )
-}
+  const handleLogOut = () => {
+    logout();
+    navigate("/login");
+  };
+
+  if (!authed) {
+    return <div></div>;
+  }
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">La Gloria de Calvillo</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li onClick={() => changePage("/dashboard")}>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <li onClick={() => changePage("/register")}>
+            <PersonAddIcon className="icon" />
+            <span>Registrar Usuario</span>
+          </li>
+          <li onClick={() => changePage("/crearHabitacion")}>
+            <span>Crear habitacion</span>
+          </li>
+          <li onClick={() => changePage("/calendar")}>
+            <span>Calendario</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <Button variant="contained" onClick={handleLogOut}>
+          Log Out
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
